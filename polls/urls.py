8d798970183f877405fd36a25a982b74fc95d630
prakharchoudhary <prakhar13.cool@gmail.com>
2016-06-20 01:36:02 +0530

    added more views

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.vote, name='vote'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 ]
